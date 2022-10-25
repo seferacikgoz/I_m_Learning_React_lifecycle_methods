1eb2bc83010fec1b4021ec08901ab8cd7eb95a62
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-26 02:59:02 +0300

    v3

--- src/components/UseEffectHook.jsx
+++ src/components/UseEffectHook.jsx
@@ -31,18 +31,36 @@ import { useState, useEffect } from "react";
 //! }, [var1, var2]); //? Dependency Array
 
 const UseEffectHook = () => {
+    const [count, setCount] = useState(0);
+    
+    //? componentDidMount
+    //! fetch, asyn-await, localStorage, setTimeout, setInterval()
+/*     useEffect(() => {
+      console.log("Mounting");  
+      setTimeout(()=> {
+        alert("Data Fetched")
+      }, 3000)
+    }, []) */
 
+     //? componentDidMount + componentDidUpdate
+ 
     useEffect(() => {
-        //? ComponentDidMount + componentDidUpdate
-    
-      return () => {
-        //? Clean-up function (componentWillMount)
-    
-      }
-    }, []) //!? Dependency Array
-    
+        console.log("Mounting + Updating");  
+        setTimeout(()=> {
+          alert("Data Fetched")
+        }, 3000)
+      }, [count])
+
+    console.log("Rendering")
+
   return (
-    <div>UseEffectHook</div>
+    <div className="container text-center">
+        <h1 className="text-danger">USE EFFECT</h1>
+        <h3>COUNT={count}</h3>
+        <button className="btn btn-info" onClick={() => setCount(count +1)}>
+          INC
+        </button>
+      </div>
   )
 }
 
