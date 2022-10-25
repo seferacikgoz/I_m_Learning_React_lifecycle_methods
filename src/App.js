6c817a88dab1e0254355811b8a5aa60d73124177
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-25 20:44:16 +0300

    I m Learning LifeCycle Methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import LifeCycleMethods from "./components/LifeCycleMethods";
 
 function App() {
+ 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center mt-4">
+      <LifeCycleMethods/>
     </div>
   );
 }
 
 export default App;
+
