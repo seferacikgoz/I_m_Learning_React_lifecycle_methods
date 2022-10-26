e5554774cc9a3d2eec15edc3b8d65a8a4fcb006f
seferacikgoz <102485608+seferacikgoz@users.noreply.github.com>
2022-10-26 22:57:03 +0300

    optional chaining

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const User = () => {
     const [user, setUser] = useState("")
 
-     fetch("https://randomuser.me/api")
-    .then((res) => res.json())
-    .then((data) => console.log(data)); 
-
-    /* console.log(user)
- */
+    const getUser = () => {
+        fetch("https://randomuser.me/api")
+       .then((res) => res.json())
+       .then((data) => setUser(data.results[0])); 
+    }
+ 
+    useEffect(() => {
+        //? componentDidMount
+        getUser()
+    }, [])
+    
+    console.log(user)
+    const {name, dob, email, picture} = user
   return (
     <div>
-    <h1>NAME</h1>
+    <h1>
+       {/*  Optional Chaining */}
+        {name?.first} {name?.last}
+    </h1>
     <img src="" alt="" />
     <p>email</p>
     <h5>DOB</h5>
-    <button className="btn btn-danger">Get User</button>
+    <button className="btn btn-danger" onClick={getUser}>Get User</button>
 
     </div>
   )
